d5c9609a14f65a7e655d9b2183b120cd6b534208
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-05-20 17:50:24 -0700

    Add files via upload

---
+++ src/Terpz710/CustomRanks/RankCommand/RanksCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terpz710\CustomRanks\RankCommand;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat as TF;
+
+use Terpz710\CustomRanks\Loader;
+
+class RankCommand extends Command {
+
+    private $plugin;
+
+    public function __construct(Loader $plugin) {
+        parent::__construct("rank");
+        $this->setDescription("Set or Remove a players rank");
+        $this->setAlias(["r", "cr", "ranks"]);
+        $this->setPermission("customranks.cmd");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args) {
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+
+        if (count($args) < 2) {
+            $sender->sendMessage(TF::RED . "Usage: /rank <set|remove|check> <player> <rank>");
+            return false;
+        }
+
+        $action = array_shift($args);
+        $playerName = array_shift($args);
+        $rank = array_shift($args);
+
+        $player = $this->plugin->getServer()->getPlayerExact($playerName);
+        if ($player === null) {
+            $sender->sendMessage(TF::RED . "Player not found!");
+            return false;
+        }
+
+        $ranksManager = $this->plugin->getRanksManager();
+
+        switch ($action) {
+            case "set":
+                if ($rank === null) {
+                    $sender->sendMessage(TF::RED . "You must specify a rank!");
+                    return false;
+                }
+                $ranksManager->setRank($player, $rank);
+                $sender->sendMessage(TF::GREEN . "Set rank of " . $playerName . " to " . $rank);
+                break;
+            case "remove":
+                $ranksManager->removeRank($player);
+                $sender->sendMessage(TF::GREEN . "Removed rank of " . $playerName);
+                break;
+            case "check":
+                $currentRank = $ranksManager->getRank($player);
+                $sender->sendMessage(TF::GREEN . $playerName . "'s current rank is " . $currentRank);
+                break;
+            default:
+                $sender->sendMessage(TF::RED . "Invalid action. Usage: /rank <set|remove|check> <player> <rank>");
+                break;
+        }
+
+        return true;
+    }
+}
