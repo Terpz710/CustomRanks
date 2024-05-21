bc052bc9435367e29f010211b38d5be5ab3e1ec1
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-05-20 17:45:54 -0700

    Add files via upload

---
+++ src/Terpz710/CustomRanks/Loader.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terpz710\CustomRanks;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat as TF;
+
+use Terpz710\CustomRanks\Command\RankCommand;
+use Terpz710\CustomRanks\RanksManager;
+
+class Loader extends PluginBase implements Listener {
+
+    private $ranksManager;
+
+    public function onEnable(): void{
+        if (!is_dir($this->getDataFolder())) {
+            mkdir($this->getDataFolder());
+        }
+
+        $this->saveResource("Ranks.yml");
+        $this->ranksManager = new RanksManager($this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->registerCommands();
+    }
+
+    public function getRanksManager(): RanksManager {
+        return $this->ranksManager;
+    }
+
+    private function registerCommands() {
+        $this->getServer()->getCommandMap()->register("rank", new RankCommand($this));
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $ranksManager = $this->getRanksManager();
+        if (!$ranksManager->getRank($player)) {
+            $ranksManager->setRank($player, $ranksManager->getDefaultRank());
+            $player->sendMessage(TF::GREEN . "Your rank has been set to " . $ranksManager->getDefaultRank() . "§a!");
+        }
+    }
+}
---
+++ src/Terpz710/CustomRanks/RankManager.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terpz710\CustomRanks;
+
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+
+use Terpz710\CustomRanks\Loader;
+
+class RanksManager {
+
+    private $plugin;
+    private $ranksData;
+    private $ranksConfig;
+    private $defaultRank;
+
+    public function __construct(Loader $plugin) {
+        $this->plugin = $plugin;
+        $this->loadRanks();
+        $this->loadRanksConfig();
+    }
+
+    private function loadRanks() {
+        $this->ranksData = (new Config($this->plugin->getDataFolder() . "Player_Ranks.json", Config::JSON))->getAll();
+    }
+
+    private function saveRanks() {
+        $config = new Config($this->plugin->getDataFolder() . "Player_Ranks.json", Config::JSON);
+        $config->setAll($this->ranksData);
+        $config->save();
+    }
+
+    private function loadRanksConfig() {
+        $this->ranksConfig = (new Config($this->plugin->getDataFolder() . "Ranks.yml", Config::YAML))->getAll();
+        $this->defaultRank = $this->ranksConfig['default_rank'] ?? null;
+    }
+
+    public function setRank(Player $player, string $rank) {
+        $this->ranksData[$player->getName()] = $rank;
+        $this->saveRanks();
+    }
+
+    public function getRank(Player $player): ?string {
+        return $this->ranksData[$player->getName()] ?? null;
+    }
+
+    public function removeRank(Player $player) {
+        if (isset($this->ranksData[$player->getName()])) {
+            unset($this->ranksData[$player->getName()]);
+            $this->saveRanks();
+        }
+    }
+
+    public function rankHierarchy(): array {
+        return $this->ranksConfig['hierarchy'] ?? [];
+    }
+
+    public function getRankPermissions(string $rank): ?array {
+        return $this->ranksConfig['ranks'][$rank]['permissions'] ?? null;
+    }
+
+    public function getDefaultRank(): ?string {
+        return $this->defaultRank;
+    }
+}
